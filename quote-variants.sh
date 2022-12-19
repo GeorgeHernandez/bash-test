74dee76e61402a4666c116fabbc16249c3bf4e47
George Hernandez <georgelhernandez@gmail.com>
2022-12-19 11:57:24 -0600

    tweak quot-variants

--- quote-variants.sh
+++ quote-variants.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # quote-variants.sh
-a=ape
-b="bat"
-c='cat'
-echo $a
+a=ape             # No quote        Expressions: No   Singles: No   Escapes: No     Spaces: No
+b='bat\t$$a'      # Single          Expressions: No   Singles: No   Escapes: No     Spaces: Yes
+c=$'cat\t$$a\''   # Single ANSI     Expressions: No   Singles: \'   Escapes: Yes    Spaces: Yes
+d="dog\t\$$a'"    # Double          Expressions: Yes  Singles: Yes  Escapes: Some   Spaces: Yes
+e=$"elf\t\$$a'"   # Double Locale   Expressions: Yes  Singles: Yes  Escapes: Some   Spaces: Yes
+echo "$a"
 echo "$b"
-echo "${c}"
-echo $a' bad'
-echo $b "bad"
-echo "${c} good" # Most consistent usage of variable in a string
+echo "$c"
+echo "$d"
+echo "$e"
+echo "$a isn't"$'\tsad'     # Strings can go side-by-side
+echo "${a^^a}boy is ${#a}"  # There are many parameter expansion options available
